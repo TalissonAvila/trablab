856c4efdcc5a284edc82402454a88250efb300ff
Talisson Freitas Avila <46037472+TalissonAvila@users.noreply.github.com>
2019-02-14 00:55:54 -0200

    Update Perceptron.c

--- Perceptron.c
+++ Perceptron.c
@@ -1,20 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <time.h>
 
 int main(void) {
-  int  novoy, cont, i;
+  int  novoy, cont,i,escolha;
   float x1[30], x2[30],y[30], w1, w2, x3, w3;
   float ProdutoInterno,acerto,porcentagem, alfa, erro;
   char linha[100];
-  
-  w1 = 0.5;
-  w2 = 0.5;
-  w3 = 0.5;
+
+  srand(time(NULL));
+  w1 = rand() % 20 - 10;
+  w2 = rand() % 20 - 10;
+  w3 = rand() % 20 - 10;
   x3 = 1.0;
   alfa = 0.01;
-  FILE *arquivo;
-  arquivo = fopen("A_I.txt","r");
+  acerto = 0;
+  i = 0;
+    printf("Escolha :\n");
+    printf("          1 para Dataset A. \n");
+    printf("          2 para Dataset B. \n");
+    scanf("%d",&escolha);
+     FILE *arquivo;
+  switch (escolha){
+    case 1 :
+   
+  arquivo = fopen("A_l.txt","r");
       	if (arquivo == NULL) {
 		     printf("Nao foi possivel abrir o arquivo.\n");
 		       exit(0);
@@ -22,37 +33,86 @@ int main(void) {
   
   while(fgets(linha, 100, arquivo) != NULL){
         fscanf(arquivo,"%f %f %f", &x1[i], &x2[i], &y[i]);
-        i++;}
-    while(cont <= 30){
+        i++;
+  }
+  fclose(arquivo);
 
-    ProdutoInterno = x1[i] * w1 + x2[i] * w2 + x3 *w3;
-    
+  do{
+    cont = 0;
+    for (i = 0;i < 30;i++){
 
-    if (ProdutoInterno >= 0){
-      novoy = 1;
-    } else novoy = 0;
-
-    erro = y[i] - novoy;
-    if (erro == 0){
-      cont++;
-    } else cont = 0;
-    w1 = w1 + (alfa * erro * x1[i]);
-    w2 = w2 + (alfa * erro * x2[i]);
-    w3 = w3 + (alfa * erro * x3);
-
-    if (novoy == y[i]){
-      acerto = acerto + 1;
+      ProdutoInterno = x1[i] * w1 + x2[i] * w2 + x3 *w3;
+      
+      if (ProdutoInterno >= 0){
+        novoy = 1;
+      } else {
+        novoy = 0;
+      }
+      erro = y[i] - novoy;
+      if (erro == 0){
+        cont++;
+      } 
+      w1 = w1 + (alfa * erro * x1[i]);
+      w2 = w2 + (alfa * erro * x2[i]);
+      w3 = w3 + (alfa * erro * x3);
       
     }
-    
-    }
-fclose(arquivo);
-    porcentagem = (acerto / 30) * 100;
-    printf("Quantidade de acertos : %.0f\n", acerto);
-    printf("Porcetagem de acertos : %.3f%%\n\n ", porcentagem);
+  }
+  while(cont < 30);
 
-    printf("\n novo w1 = %f\n",w1);
-    printf(" novo w2 = %f\n",w2); 
-    printf(" novo w3 = %f\n", w3);
+  porcentagem = (cont / 30) * 100;
+  printf("\n\nQuantidade de acertos : %d\n", cont);
+  printf("Porcetagem de acertos : %.3f%%\n\n ", porcentagem);
+
+  printf("\n novo w1 = %f\n",w1);
+  printf(" novo w2 = %f\n",w2); 
+  printf(" novo w3 = %f\n", w3);
+  break;
+  case 2 :
+  arquivo = fopen("B_l.txt","r");
+      	if (arquivo == NULL) {
+		     printf("Nao foi possivel abrir o arquivo.\n");
+		       exit(0);
+	      }
+  
+  while(fgets(linha, 100, arquivo) != NULL){
+        fscanf(arquivo,"%f %f %f", &x1[i], &x2[i], &y[i]);
+        i++;
+  }
+  fclose(arquivo);
+
+  do{
+    cont = 0;
+    for (i = 0;i < 30;i++){
+
+      ProdutoInterno = x1[i] * w1 + x2[i] * w2 + x3 *w3;
+      
+      if (ProdutoInterno >= 0){
+        novoy = 1;
+      } else {
+        novoy = 0;
+      }
+      erro = y[i] - novoy;
+      if (erro == 0){
+        cont++;
+      } 
+      w1 = w1 + (alfa * erro * x1[i]);
+      w2 = w2 + (alfa * erro * x2[i]);
+      w3 = w3 + (alfa * erro * x3);
+      
+    }
+  }
+  while(cont < 30);
+
+  porcentagem = (cont / 30) * 100;
+  printf("\n\nQuantidade de acertos : %d\n", cont);
+  printf("Porcetagem de acertos : %.3f%%\n\n ", porcentagem);
+
+  printf("\n novo w1 = %f\n",w1);
+  printf(" novo w2 = %f\n",w2); 
+  printf(" novo w3 = %f\n", w3);
+  break;
+  }
+  
 return 0;
 }
